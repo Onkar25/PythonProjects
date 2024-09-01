d64ef5ace0f6604429a077aedd39611d04eab570
Onkar Murkar <onkar.murkar@outlook.com>
2024-09-02 01:27:47 +0530

    Uploading the day 1 and 2 practice

---
+++ MyFirstPython.py
@@ -0,0 +1,33 @@
+#  String operation and Len function
+# print("Hello World")
+# input("Whats your name ?")
+
+# Datatypes
+# print(type("Crooks"))
+# print(type(123))
+# print(type(123.455))
+# print(type(False))
+
+# Type conversion
+# float()
+# int()
+# str()
+# bool()
+# print("Number of letters in your name: " + str(len(input("Enter your name"))))
+
+# Mathematical Operators +, -, *, /, // and **
+# PEMDAS - Parentheses (), Exponents **, Multiplication/Division *,/,// , Addition/Subtraction +,-
+# Left to Right operations
+# print(3 ** 3 - 3 * 3 - 3)
+
+# Number Manupulation
+# bmi = 84 / 1.65 ** 2
+# print(bmi)
+# print(round(bmi))
+# print(round(bmi, 2))
+
+# Assignment operators +=, -=, *=, /=, //=, **=
+
+# f string - f"{}"
+# bmi = 84 / 1.65 ** 2
+# print(f"Your BMI is {bmi: .2f} and is in the {bmi: .2f} category.")
